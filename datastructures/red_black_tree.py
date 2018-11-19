d043c46fb7d78b79132e1a59624e6ecd3166c78a
Bogdan Floris <bogdan.floris@gmail.com>
2018-11-20 00:54:29 +0100

    deletion, max, min, search, predecessor and successor implemented on Red Black Tree

--- datastructures/red_black_tree.py
+++ datastructures/red_black_tree.py
@@ -50,6 +50,58 @@ class RedBlackTree:
             self.in_order_walk(x.left)
             self.in_order_walk(x.right)
 
+    def minimum(self, x: RBNode=None):
+        """
+        Finds the minimum starting at x
+        :param x: root
+        :return: minimum
+        """
+        if x is None:
+            x = self.root
+        while x.left is not nil:
+            x = x.left
+        return x
+
+    def maximum(self, x: RBNode=None):
+        """
+        Finds the maximum starting at x
+        :param x: root
+        :return: maximum
+        """
+        if x is None:
+            x = self.root
+        while x.right is not nil:
+            x = x.right
+        return x
+
+    def successor(self, x: RBNode):
+        """
+        Finds the successor of x
+        :param x: root
+        :return: the successor
+        """
+        if x.right is not nil:
+            return self.minimum(x.right)
+        y = x.parent
+        while y is not nil and x is y.right:
+            x = y
+            y = y.parent
+        return y
+
+    def predecessor(self, x: RBNode):
+        """
+        Finds the predecessor of x
+        :param x: root
+        :return: the predecessor
+        """
+        if x.left is not nil:
+            return self.maximum(x.left)
+        y = x.parent
+        while y is not nil and x is y.left:
+            x = y
+            y = y.parent
+        return y
+
     def insert(self, key):
         """
         Insert the value into the tree.
@@ -75,6 +127,51 @@ class RedBlackTree:
         z.right = nil
         z.color = RED
         self._insert_fix_up(z)
+
+    def delete(self, key):
+        """
+        Deletes the key from the tree.
+        :param key: key to be deleted
+        """
+        z = self.search(self.root, key)
+        y = z
+        y_original_color = y.color
+        if z.left is nil:
+            x = z.right
+            self._transplant(z, z.right)
+        elif z.right is nil:
+            x = z.left
+            self._transplant(z, z.left)
+        else:
+            y = self.minimum(z.right)
+            y_original_color = y.color
+            x = y.right
+            if y.parent is z:
+                x.parent = y
+            else:
+                self._transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+            self._transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        if y_original_color is BLACK:
+            self._delete_fix_up(x)
+
+    def search(self, x: RBNode, key):
+        """
+        Searches for the key starting at x
+        :param x: start
+        :param key: key to be found
+        :return: the node with the key
+        """
+        if x is nil or key == x.key:
+            return x
+        if key < x.key:
+            return self.search(x.left, key)
+        else:
+            return self.search(x.right, key)
 
     def _insert_fix_up(self, z: RBNode):
         """
@@ -116,6 +213,70 @@ class RedBlackTree:
                     z.parent.parent.color = RED
                     self._left_rotate(z.parent.parent)
         self.root.color = BLACK
+
+    def _delete_fix_up(self, x: RBNode):
+        """
+        Fixes the properties of the Red Black Tree after deletion
+        :param x: starting node
+        """
+        while x is not self.root and x.color is BLACK:
+            if x is x.parent.left:
+                w = x.parent.right
+                if w.color is RED:
+                    w.color = BLACK
+                    x.parent.color = RED
+                    self._left_rotate(x.parent)
+                    w = x.parent.right
+                if w.left.color is BLACK and w.right.color is BLACK:
+                    w.color = RED
+                    x = x.parent
+                else:
+                    if w.right.color is BLACK:
+                        w.left.color = BLACK
+                        w.color = RED
+                        self._right_rotate(w)
+                        w = x.parent.right
+                    w.color = x.parent.color
+                    x.parent.color = BLACK
+                    w.right.color = BLACK
+                    self._left_rotate(x.parent)
+                    x = self.root
+            else:
+                w = x.parent.left
+                if w.color is RED:
+                    w.color = BLACK
+                    x.parent.color = RED
+                    self._right_rotate(x.parent)
+                    w = x.parent.left
+                if w.left.color is BLACK and w.right.color is BLACK:
+                    w.color = RED
+                    x = x.parent
+                else:
+                    if w.left.color is BLACK:
+                        w.right.color = BLACK
+                        w.color = RED
+                        self._left_rotate(w)
+                        w = x.parent.left
+                    w.color = x.parent.color
+                    x.parent.color = BLACK
+                    w.left.color = BLACK
+                    self._right_rotate(x.parent)
+                    x = self.root
+        x.color = BLACK
+
+    def _transplant(self, u: RBNode, v: RBNode):
+        """
+        Helper function for deletion
+        :param u: root of subtree 1
+        :param v: root of subtree 2
+        """
+        if u.parent is nil:
+            self.root = v
+        elif u is u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        v.parent = u.parent
 
     def _left_rotate(self, x: RBNode):
         """
