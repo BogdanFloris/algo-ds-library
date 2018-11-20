b772ca7787966ea3406d481d17501104ae9ef92f
Bogdan Floris <bogdan.floris@gmail.com>
2018-11-20 17:48:52 +0100

    breadth first search and print path implemented

--- algorithms/graphs/graph.py
+++ algorithms/graphs/graph.py
@@ -1,6 +1,14 @@
 """
 Graphs Module
 """
+import queue
+import sys
+
+
+# Colors used by the search algorithms
+WHITE = 0
+BLACK = 1
+GRAY = 2
 
 
 class Vertex:
@@ -8,6 +16,13 @@ class Vertex:
         self.id = key
         # map from a neighbour vertex to the weight of the edge
         self.connected_to = {}
+        # maps a distance from a source vertex to this vertex
+        # ex: dist[neighbour_1] = 4 => dist(neighbour_1, self) = 4
+        self.dist = {}
+        # color of this vertex for search algorithms, initialized as WHITE
+        self.color = WHITE
+        # predecessor, initialized as None
+        self.predecessor = None
 
     def __str__(self):
         return str(self.id) + " connected to: " + str([x.id for x in self.connected_to])
@@ -53,17 +68,9 @@ class Graph:
     def __iter__(self):
         return iter(self.vertices.values())
 
-    def connected(self, key_1, key_2):
-        """
-        :param key_1: vertex with key_1
-        :param key_2: vertex with key_2
-        :return: true if vertex 1 is connected to vertex 2, false otherwise
-        """
-        neighbours = self.vertices[key_1].get_connections()
-        for neighbour in neighbours:
-            if key_2 == neighbour.get_id():
-                return True
-        return False
+    ############################################
+    # Basic commands and queries for the Graph #
+    ############################################
 
     def get_vertex(self, key):
         """
@@ -120,6 +127,74 @@ class Graph:
             if not self.directed:
                 self.vertices[vertex_2].add_neighbour(self.vertices[vertex_1], cost[0])
 
+    ###################################
+    # Search algorithms for the graph #
+    ###################################
+
+    def connected(self, key_1, key_2):
+        """
+        :param key_1: vertex with key_1
+        :param key_2: vertex with key_2
+        :return: true if vertex 1 is connected to vertex 2, false otherwise
+        """
+        neighbours = self.vertices[key_1].get_connections()
+        for neighbour in neighbours:
+            if key_2 == neighbour.get_id():
+                return True
+        return False
+
+    def print_path(self, key_source, key_destination):
+        """
+        Prints the path from source to destination if it exists.
+        Assumes that the predecessor have been computed.
+        :param key_source: key of source vertex
+        :param key_destination: key of destination vertex
+        """
+        source = self.get_vertex(key_source)
+        destination = self.get_vertex(key_destination)
+        self._print_path_util(source, destination)
+
+    def _print_path_util(self, source, destination):
+        """
+        Main print path algorithm.
+        :param source: source vertex
+        :param destination: destination vertex
+        """
+        if source is destination:
+            print(source.get_id(), end=" ")
+        elif destination.predecessor is None:
+            print("No path from {} to {} exists!".format(source.get_id(), destination.get_id()))
+        else:
+            self._print_path_util(source, destination.predecessor)
+            print(destination.get_id(), end=" ")
+
+    def bfs(self, source_key):
+        """
+        Runs the BFS algorithm from source_key and computes
+        the shortest number of edges from the source to all
+        other vertices and predecessor of every vertex.
+        :param source_key: the key of the source vertex
+        """
+        source: Vertex = self.get_vertex(source_key)
+        for u in self:
+            if u is not source:
+                # initialize the distance from the source
+                u.dist[source] = sys.maxsize
+        source.color = GRAY
+        source.dist[source] = 0
+        # initialize queue with max size number of vertices
+        q = queue.Queue(self.num_vertices)
+        q.put(source)
+        while not q.empty():
+            u: Vertex = q.get()
+            for v in u.get_connections():
+                if v.color is WHITE:
+                    v.color = GRAY
+                    v.dist[source] = u.dist[source] + 1
+                    v.predecessor = u
+                    q.put(v)
+            u.color = BLACK
+
 
 if __name__ == "__main__":
     g = Graph()
@@ -134,7 +209,10 @@ if __name__ == "__main__":
     g.add_edge(4, 0, 1)
     g.add_edge(5, 4, 8)
     g.add_edge(5, 2, 1)
-    for v in g:
-        for w in v.get_connections():
-            print("({}, {}) with weight {}".format(v.get_id(), w.get_id(), v.get_weight(w)))
-    print(g.connected(0, 1))
+    for vertex in g:
+        for w in vertex.get_connections():
+            print("({}, {}) with weight {}".format(vertex.get_id(), w.get_id(), vertex.get_weight(w)))
+    g.bfs(0)
+    s = g.get_vertex(0)
+    for vertex in g:
+        print("Shortest number of edges from 0 to {}: {}".format(vertex.get_id(), vertex.dist[s]))
